4b193daf580872dd78166bac9d1968c3622aa042
james777888 <james777888@126.com>
2020-07-12 10:27:54 +0800

    james

--- src/main/java/com/atguigu/huashan/Dugujianfa.java
+++ src/main/java/com/atguigu/huashan/Dugujianfa.java
@@ -11,5 +11,9 @@ public class Dugujianfa {
         System.out.println("独孤剑法");
         System.out.println("独孤剑法");
         System.out.println("独孤剑法");
+
+        System.out.println("詹姆斯");
+        System.out.println("詹姆斯");
+        System.out.println("詹姆斯");
     }
 }
