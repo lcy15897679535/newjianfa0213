99da6a8823ecad690f7a5d37b51adbb9be2e3488
james777888 <james777888@126.com>
2020-07-12 21:18:27 +0800

    zsf

---
+++ src/main/java/com/atguigu/wudanshang/Taijijianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu.wudanshang;
+
+/**
+ * @author lcy
+ * @create 2020-07-12 21:17
+ */
+public class Taijijianfa {
+
+    public static void main(String[] args) {
+        System.out.println("太极剑法");
+    }
+}
