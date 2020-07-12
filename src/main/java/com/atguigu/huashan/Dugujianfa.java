a118e927553f631b91ef74fe060b5e8c788399eb
liuchongyang <835486902@qq.com>
2020-07-12 10:16:23 +0800

    jianfa

---
+++ src/main/java/com/atguigu/huashan/Dugujianfa.java
@@ -0,0 +1,15 @@
+package com.atguigu.huashan;
+
+/**
+ * @author lcy
+ * @create 2020-07-12 10:06
+ */
+public class Dugujianfa {
+
+    public static void main(String[] args) {
+        System.out.println("独孤剑法");
+        System.out.println("独孤剑法");
+        System.out.println("独孤剑法");
+        System.out.println("独孤剑法");
+    }
+}
